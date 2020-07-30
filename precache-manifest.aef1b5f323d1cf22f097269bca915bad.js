703600da1f422966f5d4209e54db57fa633baf6e
elroyi <elroyi3000@gmail.com>
2020-07-30 21:49:39 +0300

    Updates

---
+++ precache-manifest.aef1b5f323d1cf22f097269bca915bad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded1a5e1879fae8f9454ab3596c3b0b8",
+    "url": "/elroyi-bendavid-27-7-2020/index.html"
+  },
+  {
+    "revision": "b2cead7855ec0b8c2308",
+    "url": "/elroyi-bendavid-27-7-2020/static/css/main.bcf12e56.chunk.css"
+  },
+  {
+    "revision": "cae5aca79d1e8ca0f324",
+    "url": "/elroyi-bendavid-27-7-2020/static/js/2.16b2ddb1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/elroyi-bendavid-27-7-2020/static/js/2.16b2ddb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2cead7855ec0b8c2308",
+    "url": "/elroyi-bendavid-27-7-2020/static/js/main.fd206849.chunk.js"
+  },
+  {
+    "revision": "30e91a045f570390f9af",
+    "url": "/elroyi-bendavid-27-7-2020/static/js/runtime-main.1b209260.js"
+  }
+]);
