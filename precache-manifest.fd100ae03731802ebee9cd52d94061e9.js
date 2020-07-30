beceb5be4a5c189f9cdcce4c5d8c077e8f812f5e
elroyi <elroyi3000@gmail.com>
2020-07-30 23:15:42 +0300

    Updates

---
+++ precache-manifest.fd100ae03731802ebee9cd52d94061e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5446fd6146440050b015de098886430",
+    "url": "/elroyi-bendavid-27-7-2020.git/index.html"
+  },
+  {
+    "revision": "595a30a9c308d94411cc",
+    "url": "/elroyi-bendavid-27-7-2020.git/static/css/main.bcf12e56.chunk.css"
+  },
+  {
+    "revision": "a7e9a1aeb177e795232f",
+    "url": "/elroyi-bendavid-27-7-2020.git/static/js/2.2d2946ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/elroyi-bendavid-27-7-2020.git/static/js/2.2d2946ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "595a30a9c308d94411cc",
+    "url": "/elroyi-bendavid-27-7-2020.git/static/js/main.d3f2d393.chunk.js"
+  },
+  {
+    "revision": "9828594d5a37468bdebb",
+    "url": "/elroyi-bendavid-27-7-2020.git/static/js/runtime-main.7d72e1f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/elroyi-bendavid-27-7-2020/precache-manifest.aef1b5f323d1cf22f097269bca915bad.js"
+  "/elroyi-bendavid-27-7-2020.git/precache-manifest.fd100ae03731802ebee9cd52d94061e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/elroyi-bendavid-27-7-2020/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/elroyi-bendavid-27-7-2020.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
