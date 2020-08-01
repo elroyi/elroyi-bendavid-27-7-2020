3891ec3b67ed25bdd75c4f8e40ebe3e6c20d958f
elroyi <elroyi3000@gmail.com>
2020-08-02 01:43:04 +0300

    Updates

---
+++ precache-manifest.05a501b1a0831d6a12b1eb883ae6fcb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb00d4aa13d11924795e0bcc25d1b2d3",
+    "url": "/elroyi-bendavid-27-7-2020.git/index.html"
+  },
+  {
+    "revision": "b38c3a790bee2f3db029",
+    "url": "/elroyi-bendavid-27-7-2020.git/static/css/main.bcf12e56.chunk.css"
+  },
+  {
+    "revision": "a7e9a1aeb177e795232f",
+    "url": "/elroyi-bendavid-27-7-2020.git/static/js/2.2d2946ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/elroyi-bendavid-27-7-2020.git/static/js/2.2d2946ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38c3a790bee2f3db029",
+    "url": "/elroyi-bendavid-27-7-2020.git/static/js/main.77051ce6.chunk.js"
+  },
+  {
+    "revision": "9828594d5a37468bdebb",
+    "url": "/elroyi-bendavid-27-7-2020.git/static/js/runtime-main.7d72e1f7.js"
+  }
+]);
